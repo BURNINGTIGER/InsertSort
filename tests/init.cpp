c1b1815e5131e43ee9a43550c3d2725e78ddd28e
BURNINGTIGER <Aleks203467@gmail.com>
2017-06-08 10:56:15 +0300

    Create init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,8 +5,8 @@ SCENARIO("sort","[sort]")
 {
  	int array[6] = { 2 , 1, 6, 3, 7, 4 };
 	int sortedArray[6] =  { 1, 2, 3, 4, 6, 7 };
-	int* f = &matrix[0];
-	int* l = &matrix[6];
+	int* f = &array[0];
+	int* l = &array[6];
 	InsertSort(f, l);
 	bool result = true;
 	for (int i = 0; i < 6; i++)
