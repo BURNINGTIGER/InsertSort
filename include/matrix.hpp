16bcddabd7dde185c9359fb07259d1f44b6941b2
BURNINGTIGER <Aleks203467@gmail.com>
2017-06-08 09:47:06 +0300

    Create matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,26 +1,16 @@
+
+#include <algorithm>
 #include <iostream>
-#include <iomanip>
-#include <fstream>
+#include <vector>
 
 using namespace std;
 
-class Matrix
+template <typename Iterator>
+void insertion_sort(Iterator f, Iterator l)
 {
-private:
-	int **matr;
-	int st, cl;
-
-public:
-	Matrix();
-	Matrix(int, int);
-	Matrix(const Matrix &matrc);
-	~Matrix();
-	int rows()const;
-	int columns()const;
-	Matrix operator+ (const Matrix &mat_2) const;
-	Matrix operator* (const Matrix &mat_2) const;
-	Matrix& operator =(Matrix &);
-	bool operator==(const Matrix&)const;
-	friend istream& operator >> (istream& ist, Matrix& cmatr);
-	friend ostream& operator << (ostream&, const Matrix&);
-};
+	if (f > l)
+		return;
+	for (Iterator i = f + 1; i != l; ++i)
+	for (Iterator j = i; j != first && *j < *(j - 1); --j)
+		std::iter_swap(j - 1, j);
+}
